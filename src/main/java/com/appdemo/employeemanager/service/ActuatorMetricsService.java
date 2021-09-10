4d74f11f2b38752fd4a59f49bb325bec7272a3c9
boudouaromar <boudouaromar@gmail.com>
2021-09-10 14:19:19 +0100

    test

--- src/main/java/com/appdemo/employeemanager/service/ActuatorMetricsService.java
+++ src/main/java/com/appdemo/employeemanager/service/ActuatorMetricsService.java
@@ -1,7 +1,6 @@
 package com.appdemo.employeemanager.service;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.actuate.health.HealthComponent;
 import org.springframework.boot.actuate.health.HealthEndpoint;
 import org.springframework.boot.actuate.info.InfoEndpoint;
@@ -20,7 +19,7 @@ public class ActuatorMetricsService {
     private final HealthEndpoint healthEndpoint;
     private final InfoEndpoint infoEndpoint;
 
-    @Autowired
+
     public ActuatorMetricsService(MetricsEndpoint metricsEndpoint,HealthEndpoint healthEndpoint,InfoEndpoint infoEndpoint){
         this.metricsEndpoint=metricsEndpoint;
         this.healthEndpoint=healthEndpoint;
@@ -33,6 +32,13 @@ public class ActuatorMetricsService {
             log.info(n+" = "+metricsEndpoint.metric(n, Collections.emptyList()).getMeasurements());
         });
     }
+    @Scheduled(initialDelay = 6000, fixedDelay = 30000)
+    public void fetchHealth() {
+        HealthComponent health = healthEndpoint.health();
+        log.info("health = {}" , health.getStatus());
+    }
+
+
 
     @Scheduled(initialDelay = 6000, fixedDelay = 60000)
     public void fetchInfo() {
